67a5b8377a836b223f77c24b311da2fcab15e4fc
Callum Spiller <c.j.spiller@se19.qmul.ac.uk>
2021-12-09 22:11:44 +0000

    created tests for editing, amended broken revers url in models.py (#44)
    * created tests for editing, amended broken revers url in models.py
    
    * Update hobbies/tests.py
    
    * merge imports
    
    Co-authored-by: Aaron Cuthbertson <a.j.cuthbertson@se19.qmul.ac.uk>
    
    * Update hobbies/tests.py
    
    Co-authored-by: Aaron Cuthbertson <a.j.cuthbertson@se19.qmul.ac.uk>
    
    * copied the user creds
    
    Co-authored-by: Aaron Cuthbertson <a.j.cuthbertson@se19.qmul.ac.uk>
    
    * tidied up test
    
    * Revert db file
    
    Co-authored-by: Callum Spiller <callumspiller@hotmail.com>
    Co-authored-by: Aaron Cuthbertson <a.j.cuthbertson@se19.qmul.ac.uk>

--- hobbies/tests.py
+++ hobbies/tests.py
@@ -8,18 +8,31 @@ from django.test import TestCase
 from django.contrib import auth
 from django.urls import reverse_lazy
 
-from .models import User
+from .models import User, Hobby
 
 TEST_USERNAME = 'user'
 TEST_USERNAME2 = "newuser"
 TEST_PASSWORD = 'password123'
+TEST_CITY = 'Dublin'
 TEST_DOB = datetime.datetime.now()
 TEST_EMAIL = "user@example.com"
+TEST_HOBBIES = []
+
+
 CREDS = {
     "username": TEST_USERNAME,
     "password": TEST_PASSWORD,
     "dob": TEST_DOB,
     "email": TEST_EMAIL
+}
+
+EDITED_USER_CREDS = {
+    "username": TEST_USERNAME,
+    "password": TEST_PASSWORD,
+    "dob": TEST_DOB,
+    "email": TEST_EMAIL,
+    "city": TEST_CITY,
+    "hobbies": TEST_HOBBIES
 }
 
 # This basic tests is to be used as an example for running tests in S2I
@@ -116,3 +129,91 @@ class GetLogin(TestCase):
         self.assertTrue(response.context["user"].is_authenticated)
         self.assertTemplateUsed('hobbies/login.html')
         self.assertTemplateUsed('hobbies/index.html')
+
+
+class EditData(TestCase):
+    def setUp(self):
+        #Create user to edit
+        user = User.objects.create(username=TEST_USERNAME,
+                                   email=TEST_EMAIL,
+                                   dob=TEST_DOB,
+                                   city=TEST_CITY)
+
+        user.set_password(TEST_PASSWORD)
+        user.save()
+
+        self.user_id = user.id
+
+        #create hobbies to add/remove
+        Hobby.objects.create(name='Football',
+                             description='Playing Football')
+        Hobby.objects.create(name='Music',
+                             description='Making music')
+        Hobby.objects.create(name='Coding',
+                             description='Writing code')
+
+        # Get CSRF token for login
+        login_url = reverse_lazy("login")
+        response = self.client.get(login_url)
+        self.assertContains(response, 'csrfmiddlewaretoken')
+        soup = BeautifulSoup(response.content, features="html.parser")
+        csrf_token_input = soup.find('input', attrs={'name': 'csrfmiddlewaretoken'})
+        data = CREDS.copy()
+        data['csrfmiddlewaretoken'] = csrf_token_input['value']
+
+        # login and assert login
+        response = self.client.post(login_url, data, follow=True)
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(response.context["user"].is_authenticated)
+
+
+    def test_edit_user(self):
+        #get request to user page to get csrf token for test
+        data = EDITED_USER_CREDS.copy()
+        user_page_url = f"/users/{self.user_id}/" #created user from above has id of 1
+        user_page_response = self.client.get(user_page_url, data)
+        self.assertContains(user_page_response, 'csrfmiddlewaretoken')
+        soup = BeautifulSoup(user_page_response.content, features="html.parser")
+        csrf_token_input_user = soup.find('input', attrs={'name': 'csrfmiddlewaretoken'})
+
+        # #request to api with new token and new dob to update
+        data['csrfmiddlewaretoken'] = csrf_token_input_user['value']
+        data['dob'] = '2012-12-12' #arbitrary date to check
+        edit_url = f"/api/users/{self.user_id}/"
+        response = self.client.put(edit_url, data)
+        self.assertEquals(response.status_code, 200)
+
+        #check dob is updated
+        user = User.objects.get(id=self.user_id)
+        self.assertEquals(str(user.dob), '2012-12-12') #convert datetime to string that matches date string above
+
+        #update user with single new hobby
+        data['hobbies'] = [1]
+        response = self.client.put(edit_url, data)
+        self.assertEquals(response.status_code, 200)
+
+        #check user added to hobby
+        test_hobby_1 = Hobby.objects.get(id=1)
+        self.assertEquals(test_hobby_1.to_dict_with_users()['users']['ids'][0], self.user_id)
+
+        # update user with multiple hobbies
+        data['hobbies'] = [1, 2, 3] #ids of created test hobbies
+        response = self.client.put(edit_url, data)
+        self.assertEquals(response.status_code, 200)
+
+        # check user added to hobbies
+        test_hobby_2 = Hobby.objects.get(id=2)
+        test_hobby_3 = Hobby.objects.get(id=3)
+        self.assertEquals(test_hobby_1.to_dict_with_users()['users']['ids'][0], self.user_id)
+        self.assertEquals(test_hobby_2.to_dict_with_users()['users']['ids'][0], self.user_id)
+        self.assertEquals(test_hobby_3.to_dict_with_users()['users']['ids'][0], self.user_id)
+
+        #clear hobbies
+        data['hobbies'] = []
+        response = self.client.put(edit_url, data)
+        self.assertEquals(response.status_code, 200)
+
+        #check user removed from all hobbies
+        self.assertEquals(len(test_hobby_1.to_dict_with_users()['users']['ids']), 0)
+        self.assertEquals(len(test_hobby_2.to_dict_with_users()['users']['ids']), 0)
+        self.assertEquals(len(test_hobby_3.to_dict_with_users()['users']['ids']), 0)
